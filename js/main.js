6a00386fb49679191a0c40cf4bc9754fec38108f
michelepiccinno <michele.piccinno@gmail.com>
2023-07-19 18:40:28 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const app = Vue.createApp({
+
+  data() {
+    return {
+      message: "Scrivi nel riquadro",
+    }
+  },
+
+
+
+  methods: {
+
+    cambiaColoreSfondo(colore) {
+      let nuovoColore = "info";
+    },
+  },
+
+
+}
+)
+
+
+app.mount('#app')
